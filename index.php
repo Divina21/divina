18f1b3d091e90efbc5d149bdc58536d7ec383724
Divina21 <36093237+Divina21@users.noreply.github.com>
2018-02-05 13:17:04 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+
+<title>WEB DESIGN 1</title>
+</head>
+<body style="background-color:red;">
+
+<center><h1 style="background-color:pink;">PERSONAL DATA</h1></center>
+<p><h2>NAME:<u></u>Divina De Jesus</h2></p>
+<p><h2>AGE N SEX:<u>20 female</u></h2></p>
+<p><h2>ADDRESS:<u>Lutupan Toledo city </u></h2></p>
+<p><h2>DATE OF BIRTH:<u>April 12 1997</u></h2><p>
+<p><h2>CIVIL STATUS:<u>Single</u></h2></p>
+<p><h2>RELIGION:<u>INC</u></h2></p>
+<p><h2>CITIZENSHIP:<u>Filipino</u></h2></p>
+<p><h2>EMAIL ADDRESS:<u>ddivina73@yahoo.com</u></h2></p><hr>
+<center><h1 style="background-color:pink">EDUCATIONAL ATTAINMENT</h1></center>
+<p><h2>ELEMENTARY:<u>General P.del Rosario Elementary school</u></h2></p>
+<p><h2>HIGH SCHOOL:<u>Don Andres Soriano National Highschool</u></h2></p>
+<p><h2>COLLEGE:<u>Cebu Technological University</u></h2></p><hr>
+<center><h1 style="background-color:pink;">BIOGRAPHY</h1></center>
+<p><h2>I am Divina De Jesus, 20 yrs of age and now living in Lutopan, Toleda City.
+I was Graduated high school in Don Andress Soriano National High School.
+I am second amoung four siblings , after i graduarted high school, I worked for about
+two years in two different Companies and then  i decided to go back in studying beacause  
+i want to purse my dreams and help the remaining members of my family.
+And now luckily i am now first year college and hope to continue till the end .
+my motto in life is "Dont let others put you down just be yourself and be proud".</h2></p>
+</body>
+</html>
